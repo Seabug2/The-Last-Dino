d1a7ac07f087d8a023d95369a4043a01e0bab8e8
정해창 <femgoon0425@gmail.com>
2024-06-30 00:46:41 +0900

    정리

--- Assets/Floating.cs
+++ Assets/Floating.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Floating : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float frequency = 1.0f; // 진동 주파수 (초당 몇 번 움직일지)
+    public float amplitude = 1.0f; // 진동의 진폭 (최대 이동 거리)
+
+    private Vector3 startPosition;
+
+    private void Start()
     {
-        
+        // 객체의 초기 위치를 저장
+        startPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        // 시간에 따라 사인 곡선을 계산하여 Y축 위치를 변경
+        float yOffset = Mathf.Sin(Time.time * frequency) * amplitude;
+        transform.position = startPosition + new Vector3(0, yOffset, 0);
     }
 }
