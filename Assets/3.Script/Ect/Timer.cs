11ea7015dbaa86242b30d48441c5b8f6030f11fe
정해창 <femgoon0425@gmail.com>
2024-06-29 22:28:40 +0900

    [정리]

--- Assets/3.Script/Ect/Timer.cs
+++ Assets/3.Script/Ect/Timer.cs
@@ -6,10 +6,6 @@ public class Timer : MonoBehaviour
 {
     [SerializeField]
     Text timeText;
-    [SerializeField]
-    public float size1 = 25f; // 분과 초의 크기 설정 변수
-    [SerializeField]
-    public float size2 = 20f; // 밀리세컨드의 크기 설정 변수
 
     void Start()
     {
@@ -28,7 +24,7 @@ public class Timer : MonoBehaviour
         {
             yield return null;
             float elapsed = Time.time - startTime;
-            timeText.text = TimerConvert(elapsed, size1, size2);
+            timeText.text = "Time\n" + TimerConvert(elapsed);
         }
 
         GameManager.instance.score = Time.time - startTime;
@@ -36,12 +32,12 @@ public class Timer : MonoBehaviour
         Destroy(this);
     }
 
-    public static string TimerConvert(float time, float size1, float size2)
+    public static string TimerConvert(float time)
     {
         int minutes = (int)time / 60;
         int seconds = (int)time % 60;
         int milliseconds = (int)(time * 100) % 100;
 
-        return $"<size={size1}>{minutes:00}:{seconds:00}</size><size={size2}>.{milliseconds:00}</size>";
+        return $"{minutes:00}:{seconds:00}<size=25>.{milliseconds:00}</size>";
     }
 }
